7641fd8fea9c108efea8fc26d80cc5d7658f0c69
Cassie Rosner <cerosner@gmail.com>
2019-08-26 23:35:33 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Frank Rosner - Candidate for Sixth Ward Council`,
+    description: `Official campaign website for candidate Frank Rosner, running for the Sixth Ward council seat of Hoboken, New Jersey`,
+    author: `Cassie Rosner`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+}
---
+++ src/components/subheader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SubHeader = () => (
+  <h2>
+    <img src="/" alt="" />
+    <p>
+      There is no time like the present to bring positive change to Hoboken's 6th ward and the City. I am running for Hoboken's 6th Ward Council seat to unite a divided city. Together, we will make representation a core principal.
+    </p>
+  </h2>
+)
+
+export default SubHeader
