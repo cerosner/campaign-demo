3443b05b021072de491ed4a1daef9066c3b0ddfd
Cassie Rosner <cerosner@gmail.com>
2019-09-11 17:18:38 -0400

    updates index with modified components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
+import Layout from '../components/layout'
+import VotingInfo from '../components/voting-info'
+import AboutMe from '../components/about-me'
+import Issues from '../components/issues'
+import PressReleases from '../components/press-releases'
+import Contact from '../components/contact'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{
-      maxWidth: `300px`,
-      marginBottom: `1.45rem`
-    }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="" />
+    <hr />
+    <AboutMe />
+    <hr />
+    <Issues />
+    <hr />
+    <VotingInfo />
+    <hr />
+    <PressReleases />
+    <hr />
+    <Contact />
   </Layout>
 )
 
