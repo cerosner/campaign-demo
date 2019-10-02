9b9292e59daf7f42c44355c7fb703bcf43d189f4
Cassie Rosner <cerosner@gmail.com>
2019-10-01 21:29:02 -0400

    added images

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
+import facebook from '../images/facebook-logo.png'
+import twitter from '../images/twitter-logo.png'
+import  instagram from '../images/instagram-logo.png'
+
 const Socials = () => (
   <div id="social-media">
-    <a href="/">
-      <img src="/" className="icon-header" alt="Facebook" />
+    <h5>Connect with me!</h5>
+    <div id="social-links">
+    <a href="https://www.facebook.com/FMROZforHoboken6thWard/" target="_blank">
+      <img src={facebook} className="icon-header" alt="Facebook" /><br />
+      <strong>Facebook</strong>
     </a>
-    <a href="/">
-      <img src="/" className="icon-header" alt="Instagram" />
+    <a href="https://www.instagram.com/fmrosner/" target="_blank">
+      <img src={instagram} className="icon-header" alt="Instagram" /><br />
+      <strong>Instagram</strong>
     </a>
-    <a href="/">
-      <img src="/" className="icon-header" alt="Twitter" />
+    <a href="https://twitter.com/rozone1" target="_blank">
+      <img src={twitter} className="icon-header" alt="Twitter" /><br />
+      <strong>Twitter</strong>
     </a>
+    </div>
   </div>
 )
 
--- src/components/voting-info.js
+++ src/components/voting-info.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import sticker from '../images/ivoted-sticker.png'
+
 const VotingInfo = () => (
   <div id="register">
     <div className="panel-register">
       <h3>Are you Registered to Vote?</h3>
-      <img src="/" id="img-register" alt="'I Voted!' sticker" />
+      <img src={sticker} id="img-register" alt="'I Voted!' sticker" />
     </div>
     <div className="panel-register">
       <h3>Here's what you need to know...</h3>
