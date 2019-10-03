4d819542d99bc81324dd1c2ea62fed04f7298a5b
Cassie Rosner <cerosner@gmail.com>
2019-10-02 21:02:48 -0400

    updates favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#000000`,
+        background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/hoboken-seal.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
