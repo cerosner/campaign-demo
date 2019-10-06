e2690233e980c8cf604694e266dd70516329ef73
Cassie Rosner <cerosner@gmail.com>
2019-10-05 21:32:07 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const SubHeader = () => (
     <p>
       There is no time like the present to bring positive change to Hoboken's 6th ward and the City.
       I am running for Hoboken's 6th Ward Council seat to unite a divided city.
-      Together, we will make representation a core principal.
+      Together, we will make representation a core principle.
     </p>
   </div>
 )
