0c24338f63ebf033840f7e2f752ce8e1978b5596
Cassie Rosner <cerosner@gmail.com>
2019-10-06 20:40:42 -0400

    updates voting info

--- src/components/voting-info.js
+++ src/components/voting-info.js
@@ -15,12 +15,6 @@ const VotingInfo = () => (
           Election day is <em>Tuesday, November 5th, 2019</em>.
         </li>
         <li>
-          The 6th Ward voting center is located at <em>Demarest School, 401 Garden Street</em>.
-        </li>
-        <li class="sub-li">
-          Not in our ward? <a href="https://voter.njsvrs.com/PublicAccess/servlet/com.saber.publicaccess.control.PublicAccessNavigationServlet?USERPROCESS=PollingPlace#" target="_blank">Click here</a> to locate your polling place.
-        </li>
-        <li>
           <a href="https://bit.ly/1dtHdob" target="_blank">Click here</a> to check if you're already registered to vote.
         </li>
         <li>
@@ -28,6 +22,9 @@ const VotingInfo = () => (
         </li>
         <li>
           Be sure to register by <em>Tuesday, October 15th, 2019</em>.
+          <li>
+          <a href="https://voter.njsvrs.com/PublicAccess/servlet/com.saber.publicaccess.control.PublicAccessNavigationServlet?USERPROCESS=PollingPlace#" target="_blank">Click here</a> to locate your district's polling center.
+        </li>
         </li>
       </ul>
     </div>
