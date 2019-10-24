68723b78ae0a3dc274373cc5f06c13370e20d093
Cassie Rosner <cerosner@gmail.com>
2019-10-23 23:42:17 -0400

    adds video

---
+++ src/components/video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => (
+  <div id="video">
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/EC1JKqjd8lI"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+ </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import SEO from '../components/seo'
 import Header from '../components/header'
+import Video from '../components/video'
 import VotingInfo from '../components/voting-info'
 import AboutMe from '../components/about-me'
 import PressReleases from '../components/press-releases'
@@ -13,6 +14,7 @@ import mystyles from '../styles/index.scss'
 const IndexPage = () => (
   <div id="index">
     <SEO title="Frank Rosner" />
+    <Video />
     <Header />
     <VotingInfo />
     <AboutMe />
